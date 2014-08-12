562406540b80bee47af9e2bb4c5e7475f60e895d
Chris Cooper <chris.cooper@nicta.com.au>
2014-08-12 16:38:28 +1000

    subgrids working but not coloured

--- index.js
+++ index.js
@@ -63,19 +63,39 @@ var terrainProvider = new Cesium.CesiumTerrainProvider({
   url : '//cesiumjs.org/smallterrain'
 });
 
+function subdivide(r, iterations) {
+  var x, y;
+  var result = [];
+  for (y = 0; y < iterations; ++y) {
+    var starty = y / iterations;
+    var stopy = (y + 1) / iterations;
+    var dy = r.north - r.south;
+    for (x = 0; x < iterations; ++x) {
+      var startx = x / iterations;
+      var stopx = (x + 1) / iterations;
+      var dx = r.east - r.west;
+      result.push({
+        west : r.west + startx * dx,
+        south : r.south + starty * dy,
+        east : r.west + stopx * dx,
+        north : r.south + stopy * dy
+      });
+    }
+  }
+  return result;
+}
+
 BlockTileProvider.prototype.loadTile = function(context, frameState, tile) {
   if (tile.state === Cesium.QuadtreeTileLoadState.START) {
 
-    var r = JSON.parse(JSON.stringify(tile.rectangle));
+    var rect = JSON.parse(JSON.stringify(tile.rectangle));
 
     // Avoid poles
-    r.north *= 0.99;
-    r.south *= 0.99;
+    rect.north *= 0.99;
+    rect.south *= 0.99;
 
-    var clon = (r.east + r.west) * 0.5;
-    var clat = (r.north + r.south) * 0.5;
-    
-    var createTileGeo = function(height, c) {
+    var createTileGeo = function(rs, hs, cs) {
+
       tile.data = {
         primitive : undefined,
         freeResources : function() {
@@ -86,23 +106,28 @@ BlockTileProvider.prototype.loadTile = function(context, frameState, tile) {
         }
       };
 
+      // Work out a single tint value: TODO: have these as uniforms
       var quantize = function(v) {
         return Math.floor(v * colourQuantize) / colourQuantize;
-      }
-      var color = Cesium.Color.fromBytes(c[0], c[1], c[2], 255);
-      var tint = 'vec3(' + quantize(color.red) + ',' + quantize(color.green) + ',' + quantize(color.blue) + ')';
+      };
+      var c = cs[0];
+      var tintColour = Cesium.Color.fromBytes(c[0], c[1], c[2], 255);
+      var tint = 'vec3(' + quantize(tintColour.red) + ',' + quantize(tintColour.green) + ',' + quantize(tintColour.blue) + ')';
 
-      
       tile.data.primitive = new Cesium.Primitive({
-        geometryInstances : new Cesium.GeometryInstance({
-          geometry : new Cesium.RectangleGeometry({
-            rectangle : r,
-            granularity : Cesium.Math.RADIANS_PER_DEGREE * granularityMultiplier,
-            height : height
-          }),
-          attributes : {
-            color : Cesium.ColorGeometryInstanceAttribute.fromColor(color)
-          }
+        geometryInstances : rs.map(function(r, i) {
+          var c = cs[i];
+          var color = Cesium.Color.fromBytes(c[0], c[1], c[2], 255);
+          return new Cesium.GeometryInstance({
+            geometry : new Cesium.RectangleGeometry({
+              rectangle : r,
+              granularity : Cesium.Math.RADIANS_PER_DEGREE * granularityMultiplier,
+              height : hs[i]
+            }),
+            attributes : {
+              color : Cesium.ColorGeometryInstanceAttribute.fromColor(color)
+            }
+          });
         }),
         appearance : new Cesium.MaterialAppearance({
           material : new Cesium.Material({
@@ -116,26 +141,25 @@ BlockTileProvider.prototype.loadTile = function(context, frameState, tile) {
                 }
               },
               components : {
-                diffuse : 'texture2D(image, materialInput.st).rgb * ' + tint
+                diffuse : 'texture2D(image, materialInput.st).rgb  * ' + tint
               }
             }
           })
         })
       });
-      
-      //if (height > 0) {
-      if(1){
+
+      // if (height > 0) {
+      if (1) {
         tile.data.sides = new Cesium.Primitive({
-          geometryInstances : new Cesium.GeometryInstance({
-            geometry : Cesium.WallGeometry.fromConstantHeights({
-              granularity : Cesium.Math.RADIANS_PER_DEGREE * granularityMultiplier,
-              positions : Cesium.Cartesian3.fromRadiansArray([ r.west, r.south, r.west, r.north, r.east, r.north, r.east, r.south, r.west, r.south ]),
-              maximumHeight : height,
-              minimumHeight : 0.0
-            }),
-            attributes : {
-              color : Cesium.ColorGeometryInstanceAttribute.fromColor(color)
-            }
+          geometryInstances : rs.map(function(r, i) {
+            return new Cesium.GeometryInstance({
+              geometry : Cesium.WallGeometry.fromConstantHeights({
+                granularity : Cesium.Math.RADIANS_PER_DEGREE * granularityMultiplier,
+                positions : Cesium.Cartesian3.fromRadiansArray([ r.west, r.south, r.west, r.north, r.east, r.north, r.east, r.south, r.west, r.south ]),
+                maximumHeight : hs[i],
+                minimumHeight : 0.0
+              })
+            });
           }),
           appearance : new Cesium.MaterialAppearance({
             material : new Cesium.Material({
@@ -154,41 +178,36 @@ BlockTileProvider.prototype.loadTile = function(context, frameState, tile) {
         });
       }
 
-      // tile.data.primitive.material = Cesium.Material.fromType('Checkerboard');
-
-      tile.data.boundingSphere3D = Cesium.BoundingSphere.fromRectangle3D(r);
-      tile.data.boundingSphere2D = Cesium.BoundingSphere.fromRectangle2D(r, frameState.mapProjection);
+      tile.data.boundingSphere3D = Cesium.BoundingSphere.fromRectangle3D(rect);
+      tile.data.boundingSphere2D = Cesium.BoundingSphere.fromRectangle2D(rect, frameState.mapProjection);
       Cesium.Cartesian3.fromElements(tile.data.boundingSphere2D.center.z, tile.data.boundingSphere2D.center.x, tile.data.boundingSphere2D.center.y,
           tile.data.boundingSphere2D.center);
+    };
 
-    }
-
-    function getFauxTerrain(positions) {
-      var result = Cesium.when.defer();
-      result.resolve(positions.map(function(p) {
-        return {
-          height : Math.random() * 10000
-        };
-      }));
-      return result;
-    }
+    var rects = subdivide(rect, 2);
+    var centroids = rects.map(function(r) {
+      return Cesium.Rectangle.center(r);
+    });
 
     // Query the terrain height of two Cartographic positions
-    var positions = [ Cesium.Cartographic.fromRadians((r.west + r.east) * 0.5, (r.north + r.south) * 0.5) ];
-    var promise = Cesium.sampleTerrain(terrainProvider, Math.max(0, tile.level - 1), positions);
-    //var promise = getFauxTerrain(positions);
+    var positions = [ Cesium.Cartographic.fromRadians((rect.west + rect.east) * 0.5, (rect.north + rect.south) * 0.5) ];
+    var promise = Cesium.sampleTerrain(terrainProvider, Math.max(0, tile.level - 1), centroids);
 
     Cesium.when(imageReady, function() {
-      var nx = clon / Math.PI * 0.5 + 0.5;
-      var ny = 1.0 - (clat / Math.PI + 0.5);
-      var x = Math.floor(nx * 1024);
-      var y = Math.floor(ny * 512);
-      var c = texContext.getImageData(x, y, 1, 1).data;
+
+      // Sample colours from our image
+      var colours = centroids.map(function(c) {
+        var nx = c.longitude / Math.PI * 0.5 + 0.5;
+        var ny = 1.0 - (c.latitude / Math.PI + 0.5);
+        var x = Math.floor(nx * 1024);
+        var y = Math.floor(ny * 512);
+        return texContext.getImageData(x, y, 1, 1).data;
+      });
 
       Cesium.when(promise, function(updatedPositions) {
-        var height = updatedPositions[0].height;
-        height = height * heightScale;
-        createTileGeo(height, c);
+        createTileGeo(rects, updatedPositions.map(function(p) {
+          return p.height * heightScale;
+        }), colours);
       });
 
     });
@@ -239,8 +258,7 @@ BlockTileProvider.prototype.computeDistanceToTile = function(tile, frameState) {
     boundingSphere = tile.data.boundingSphere2D;
   }
 
-  return Math.max(0.0, Cesium.Cartesian3.magnitude(Cesium.Cartesian3.subtract(boundingSphere.center, frameState.camera.positionWC, subtractScratch))
-      - boundingSphere.radius);
+  return Math.max(0.0, Cesium.Cartesian3.magnitude(Cesium.Cartesian3.subtract(boundingSphere.center, frameState.camera.positionWC, subtractScratch)) - boundingSphere.radius);
 };
 
 BlockTileProvider.prototype.isDestroyed = function() {
